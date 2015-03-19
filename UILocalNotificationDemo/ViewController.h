3ade898ecc86d65ddc2144401c52b21cd4035d55
starmier <lyg1989yyb@163.com>
2015-03-19 21:57:45 +0800

    Initial Commit

---
+++ UILocalNotificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILocalNotificationDemo
+//
+//  Created by LYG on 15/3/19.
+//  Copyright (c) 2015年 cndatacom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UILocalNotificationDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UILocalNotificationDemo
+//
+//  Created by LYG on 15/3/19.
+//  Copyright (c) 2015年 cndatacom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
